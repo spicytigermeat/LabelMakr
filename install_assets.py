9f4fb7922c0350118837c226987544310e49a218
tyler <103609620+spicytigermeat@users.noreply.github.com>
2024-03-13 21:32:43 -0500

    Update install_assets.py

--- install_assets.py
+++ install_assets.py
@@ -1,9 +1,4 @@
 import gdown
 import zipfile
-
-url = "1kQ2gSqAJsSsQB5KiATIJ4lRGPQp7_GvB"
-output = "sofa_gui_asset_pack.zip"
-gdown.download(id=url, output=output, quiet=False)
-
-with zipfile.ZipFile('sofa_gui_asset_pack.zip', 'r') as archive:
+with zipfile.ZipFile('labelmakr_assets.zip', 'r') as archive:
 	archive.extractall('.')
